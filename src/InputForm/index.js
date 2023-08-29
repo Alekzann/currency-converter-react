4954d2522650bacf75b21c51c94ea49be3a40ca4
Oleksandr Riaboshapka <hadion@mail.ru>
2023-08-29 19:48:03 +0200

    useState amount added

--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -1,18 +1,24 @@
+import {useState} from 'react';
 import "./style.css";
 
 
 
-const InputForm = () => (
+const InputForm = () => {
+    const [amount, setAmount] = useState("");
+    
+    return (
     <input 
     className="form__amount"
     name="amount"
     type="number"
     min="1"
-    value="1"
+    value={amount}
     step="any"
-    required />
-    
-);
+    placeholder="Ilość w PLN"
+    onChange={({target}) => setAmount(target.value)}
+    />
+    );
+};
 
 
 export default InputForm;
