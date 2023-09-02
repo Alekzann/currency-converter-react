0fdcd57e3746b0dca5898a9d0ce0f9a5e1a493cb
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-02 14:21:17 +0200

    select react interaction added

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,18 +1,24 @@
+import { currencies } from '../currencies.js';
 import { useState } from 'react';
+import "./style.css";
 
+const Select = () => {
+	const [currency, setCurrency] = useState(currencies[0]);
 
-
-const Select = ({currencies, onSelectChange}) => {
-	
-	
 	return (
-		<select value={currencies} onChange={onSelectChange}>
-				{currencies.map(currency => (
-					<option key={currency.id}>
-						{currency.name}
-					</option>
-				))
-				}
+		<select
+			className="select"
+			value={currency}
+			onChange={({ target }) => setCurrency(target.value)}
+		>
+			{currencies.map(currency => (
+				<option
+					key={currency.id}
+					value={currency.id}>
+					{currency.name}
+				</option>
+			))
+			}
 		</select>
 	)
 }
