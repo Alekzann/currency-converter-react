3ef2743a36a19dc79c58feb1e14e5dcd5a75b09d
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-04 14:56:33 +0200

    useStates and calculateResult added

--- src/App.js
+++ src/App.js
@@ -4,38 +4,52 @@ import Button from "./Button";
 import Result from "./Result";
 import Form from "./Form";
 import Container from "./Container";
-//import { currencies } from './currencies.js';
-
+import { currencies } from './currencies.js';
+import { useState } from 'react';
 
 
 function App() {
+  const [currency, setCurrency] = useState(currencies[0].id);
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState();
 
-  
+  const calculateResult = (amount, currency) => {
+    const rate = currencies.find(({ id }) => id === currency).rate;
 
-
+    setResult({
+      sourceAmount: +amount,
+      sourceResult: amount/rate,
+      currency,
+    })
+  }
 
   const onFormSubmit = (e) => {
     e.preventDefault();
-    
+    calculateResult(amount, currency);
   };
 
   return (
     <Container>
       <Form
         onFormSubmit={onFormSubmit}
-        titleInput="Wpisz kwotę do przeliczenia*"
-        titleSelect="Wybierz na co chcesz przeliczyć:"
+        titleInput="Kwota w PLN*"
+        titleSelect="Waluta:"
         input={
           <InputForm
+            amount={amount}
+            setAmount={setAmount}
           />}
         select={
           <Select
-            />}
+            currency={currency}
+            setCurrency={setCurrency}
+          />}
         button={
           <Button
           />}
         result={
           <Result
+          result={result}
           />}
       />
     </Container>
--- src/Select/index.js
+++ src/Select/index.js
@@ -1,26 +1,23 @@
 import { currencies } from '../currencies.js';
-import { useState } from 'react';
+
 import "./style.css";
 
-const Select = () => {
-	const [currency, setCurrency] = useState(currencies[0]);
+const Select = ({ currency, setCurrency }) => (
+	<select
+		className="select"
+		value={currency}
+		onChange={({ target }) => setCurrency(target.value)}
+	>
+		{currencies.map(currency => (
+			<option
+				key={currency.id}
+				value={currency.id}>
+				{currency.name}
+			</option>
+		))
+		}
+	</select>
+)
 
-	return (
-		<select
-			className="select"
-			value={currency}
-			onChange={({ target }) => setCurrency(target.value)}
-		>
-			{currencies.map(currency => (
-				<option
-					key={currency.id}
-					value={currency.id}>
-					{currency.name}
-				</option>
-			))
-			}
-		</select>
-	)
-}
 
 export default Select;
