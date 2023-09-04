99b9eb8b551b62f57062e93d6f10607fc8ea37ae
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-04 14:56:56 +0200

    rates actualization

--- src/currencies.js
+++ src/currencies.js
@@ -1,12 +1,12 @@
 export const currencies =
   [
     { id: "USD", name: "Dollar Amerykański", rate: 4 },
-    { id: "EUR", name: "EURO", rate: 4.5 },
+    { id: "EUR", name: "Euro", rate: 4.5 },
     { id: "CZK", name: "Czeska krona", rate: 0.2 },
     { id: "UAH", name: "Ukrainska hrywnia", rate: 0.11 },
   ];
 
 
 
-  
+
 
