60b6eabd407e4123cfc3e2764e07a6ed204bafa4
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-04 14:58:09 +0200

    Result interaction added

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,7 +1,16 @@
 import "./style.css"
 
-const Result = () => (
-    <p className="form__resultTogether">Kwota: <strong> N/A
-    </strong></p>)
+const Result = ({ result }) => (
+	<p className="result">
+		{result !== undefined && (
+			<>
+				{result.sourceAmount} {"PLN"}
+				<strong> {"= "}
+					{result.sourceResult.toFixed(2)} {result.currency}
+				</strong>
+			</>
+		)}
+	</p>
+)
 
 export default Result;
