d8a96bc86edcb1d9c889d334a51224d5cbbc079e
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-04 14:57:40 +0200

    formatting of code

--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -1,24 +1,19 @@
-import {useState} from 'react';
 import "./style.css";
 
-
-
-const InputForm = () => {
-    const [amount, setAmount] = useState("");
-    
-    return (
-    <input 
-    className="form__amount"
-    name="amount"
-    type="number"
-    min="1"
-    value={amount}
-    step="any"
-    placeholder="Ilość w PLN"
-    onChange={({target}) => setAmount(target.value)}
+const InputForm = ({ amount, setAmount }) => (
+    <input
+        className="input"
+        name="amount"
+        type="number"
+        min="1"
+        max="999999999"
+        value={amount}
+        step="any"
+        required
+        placeholder="Ilość w PLN"
+        onChange={({ target }) => setAmount(target.value)}
     />
-    );
-};
+);
 
 
 export default InputForm;
