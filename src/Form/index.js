5f467e1fd706aa08f557e7947ccadb60d066c268
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-06 12:15:21 +0200

    adding to form code from folders Button and Result

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,36 @@
 import "./style.css"
 
-const Form = ({titleInput, titleSelect, input, select, button, result, onFormSubmit}) => (
-    <form className="form " onSubmit={onFormSubmit}>
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kantor walut</legend>
-            <div>
-                <span className="form__listHeader">{titleInput}</span>
-                {input}
-            </div>
-            <div>
-                <span className="form__listHeader">{titleSelect}</span>
-                {select}
-            </div>
-            {button}
-            {result}
-        </fieldset>
-    </form>
+const Form = ({ input, select, result, onFormSubmit }) => (
+	<form className="form" onSubmit={onFormSubmit}>
+		<fieldset className="form__fieldset">
+			<legend className="form__legend">Kantor walut</legend>
+			<div className="form__container">
+				<span className="form__listHeader">"Kwota w PLN*"</span>
+				{input}
+			</div>
+			<div className="form__container">
+				<span className="form__listHeader">Waluta:</span>
+				{select}
+			</div>
+			<p>
+				<button className="form__button">
+					Przelicz
+				</button>
+			</p>
+			<div >
+				<p>
+					{result !== undefined && (
+						<>
+							{result.sourceAmount} {"PLN"}
+							<strong> {"= "}
+								{result.sourceResult.toFixed(2)} {result.currency}
+							</strong>
+						</>
+					)}
+				</p>
+			</div>
+		</fieldset>
+	</form>
 )
 
 
--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
 const InputForm = ({ amount, setAmount }) => (
-    <input
-        className="input"
-        name="amount"
-        type="number"
-        min="1"
-        max="999999999"
-        value={amount}
-        step="any"
-        required
-        placeholder="Ilość w PLN"
-        onChange={({ target }) => setAmount(target.value)}
-    />
+	<input
+		className="input"
+		name="amount"
+		type="number"
+		min="1"
+		max="999999999"
+		value={amount}
+		step="any"
+		required
+		placeholder="Ilość w PLN"
+		onChange={({ target }) => setAmount(target.value)}
+	/>
 );
 
 
