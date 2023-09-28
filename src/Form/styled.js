5932506cafdb15e0f539325dcbfabd7c1cc89303
Oleksandr Riaboshapka <hadion@mail.ru>
2023-09-28 19:08:25 +0200

    theme added to Form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const FormStyle = styled.form`
+export const theme = {
+  primaryColor: "#e5f8fb",
+  primaryTextcolor: "#455b65",
+};
+
+export const StyleForm = styled.form`
   margin: 50px auto;
   padding-top: 100px;
 
@@ -22,7 +27,7 @@ export const Fieldset = styled.fieldset`
 `;
 
 export const Legend = styled.legend`
-  color: #455b65;
+  color: ${({ theme }) => theme.primaryTextcolor};
   background-color: whitesmoke;
   padding: 5px;
   border-radius: 10px;
@@ -40,6 +45,7 @@ export const FormContainer = styled.div`
 `;
 
 export const ListHeader = styled.span`
+  color: ${({ theme }) => theme.primaryTextcolor};
   margin: 15px;
   padding: 10px;
 
@@ -49,12 +55,12 @@ export const ListHeader = styled.span`
 `;
 
 export const Input = styled.input`
-  max-width: 150px;
-  background-color: rgb(229, 248, 251);
-  color: #455b65;
+  max-width: 200px;
+  background-color: ${({ theme }) => theme.primaryColor};
+  color: ${({ theme }) => theme.primaryTextcolor};
   border: 1px dotted #455b65;
   border-radius: 5px;
-
+  
   @media(max-width: 560px) {
     margin: 0 auto;
   };
@@ -62,8 +68,9 @@ export const Input = styled.input`
 
 export const Select = styled.select`
   border-radius: 5px;
-  background-color: rgb(229, 248, 251);
+  background-color: ${({ theme }) => theme.primaryColor};
   text-align: center;
+  color: ${({ theme }) => theme.primaryTextcolor};
 
   @media(max-width: 560px) {
     text-align: center;
@@ -78,7 +85,7 @@ export const ButtonContainer = styled.div`
 export const Button = styled.button`
   width: 200px;
   padding: 5px;
-  color: #461d1d;
+  color: ${({ theme }) => theme.primaryTextcolor};
   background-color: rgb(36, 209, 209);
   border: none;
   border-radius: 5px;
