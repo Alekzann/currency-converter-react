4d361f6bb26a76e6edf4baf337cd7400fa56ff00
Oleksandr Riaboshapka <hadion@mail.ru>
2023-12-07 15:36:49 +0100

    currencies.js removed as no need

--- src/currencies.js
+++
@@ -1,7 +0,0 @@
-export const currencies =
-  [
-    { id: "EUR", name: "EURO", rate: 4.47 },
-    { id: "USD", name: "Dollar Amerykański", rate: 4.13 },
-    { id: "CZK", name: "Czeska korona", rate: 0.19 },
-    { id: "UAH", name: "Ukrainska hrywnia", rate: 0.11 },
-  ];
