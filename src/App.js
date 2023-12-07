bf417f52bae634259f13563b2a59b51a430c6721
Oleksandr Riaboshapka <hadion@mail.ru>
2023-12-07 15:36:25 +0100

    calculateResult,setResult,onFormSubmit removed to Form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import Form from "./Form";
+import {Form} from "./Form";
 import Container from "./Container/styled.js";
-import { currencies } from "./currencies.js";
-import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import {theme} from "./styled.js"
 
 function App() {
-  const [currency, setCurrency] = useState(currencies[0].id);
-  const [amount, setAmount] = useState("");
-  const [result, setResult] = useState();
-
-  const calculateResult = (amount, currency) => {
-    const rate = currencies.find(({ id }) => id === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      sourceResult: amount / rate,
-      currency,
-    });
-  };
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-    calculateResult(amount, currency);
-  };
-
   return (
-    <Container>
-      <Form
-        onFormSubmit={onFormSubmit}
-        amount={amount}
-        setAmount={setAmount}
-        currency={currency}
-        setCurrency={setCurrency}
-        result={result}
-      />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
 
