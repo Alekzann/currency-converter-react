76f0442df63cfa988977323a0b8d78f1037c50f2
Oleksandr Riaboshapka <hadion@mail.ru>
2023-12-17 16:48:40 +0100

    date parameters changed changed time in setTimeout, comments deleted

--- src/Form/useCurrencyData.js
+++ src/Form/useCurrencyData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-//const url ="https://api.currencyapi.com/v3/latest?apikey=cur_live_P1pYmdV11fvj0mAT7NTqT0wpDnsSaolNjUowOHkI&base_currency=PLN";
+const url ="https://api.currencyapi.com/v3/latest?apikey=cur_live_P1pYmdV11fvj0mAT7NTqT0wpDnsSaolNjUowOHkI&base_currency=PLN";
 
 export const useCurrencyData = () => {
   const [currencyData, setCurrencyData] = useState({
@@ -13,12 +13,12 @@ export const useCurrencyData = () => {
     setTimeout(() => {
       (async () => {
         try {
-          const response = await axios.get("/currencies.json");
-          // console.log(response.data);
+          const response = await axios.get(url);
 
           setCurrencyData({
             status: "success",
             ratesData: response.data.data,
+            date: response.data.meta.last_updated_at,
           });
         } catch {
           setCurrencyData({
@@ -26,8 +26,8 @@ export const useCurrencyData = () => {
           });
         }
       })();
-    }, 1500);
-  });
+    }, 700);
+  }, []);
 
   return { currencyData };
 };
