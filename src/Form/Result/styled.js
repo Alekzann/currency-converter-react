5eb06388d5d93d902d97627e251bce74cb67a322
Oleksandr Riaboshapka <hadion@mail.ru>
2023-12-18 23:09:12 +0100

    stylization of Result changed

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-export const StyleResult = styled.div`
-  display: flex;
-  justify-content: center;
+export const StyleResult = styled.p`
+  text-align: center;
   color: ${({ theme }) => theme.colors.primaryTextcolor};
+  margin: 20px; 
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     text-align: center;
